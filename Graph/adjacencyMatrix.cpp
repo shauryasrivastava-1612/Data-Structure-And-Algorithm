56a4401a83636e8f6ccfda2f78aeda5b98e6e573
shauryasrivastava-1612 <shauryasrivastava-1612@users.noreply.github.com>
2024-12-27 16:40:22 +0530

    added introduction.txt

--- Graph/adjacencyMatrix.cpp
+++ Graph/adjacencyMatrix.cpp
@@ -2,7 +2,55 @@
 #include <vector>
 using namespace std;
 
-// Adjency Matrix
+// Adjacency Matrix Representation of Graph Data Structure:
+// C++ program to demonstrate Adjacency Matrix
+// representation of undirected and unweighted graph
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// void addEdge(vector<vector<int>> &mat, int i, int j)
+// {
+//     mat[i][j] = 1;
+//     mat[j][i] = 1; // Since the graph is undirected
+// }
+
+// void displayMatrix(vector<vector<int>> &mat)
+// {
+//     int V = mat.size();
+//     for (int i = 0; i < V; i++)
+//     {
+//         for (int j = 0; j < V; j++)
+//             cout << mat[i][j] << " ";
+//         cout << endl;
+//     }
+// }
+
+// int main()
+// {
+//     // Create a graph with 4 vertices and no edges
+//     // Note that all values are initialized as 0
+//     int V = 4;
+//     vector<vector<int>> mat(V, vector<int>(V, 0));
+
+//     // Now add edges one by one
+//     addEdge(mat, 0, 1);
+//     addEdge(mat, 0, 2);
+//     addEdge(mat, 1, 2);
+//     addEdge(mat, 2, 3);
+
+//     /* Alternatively we can also create using below
+//        code if we know all edges in advacem
+
+//      vector<vector<int>> mat = {{ 0, 1, 0, 0 },
+//                                { 1, 0, 1, 0 },
+//                                { 0, 1, 0, 1 },
+//                                { 0, 0, 1, 0 } }; */
+
+//     cout << "Adjacency Matrix Representation" << endl;
+//     displayMatrix(mat);
+
+//     return 0;
+// }
 // TC - O(v^2) | SC - O(v^2)
 // v - vertices
 
